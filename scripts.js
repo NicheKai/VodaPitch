0ca0b7a9543afec1d3563fdb34d300ca8577658b
Niche <brendonhallows@googlemail.com>
2026-02-12 14:39:13 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -127,7 +127,7 @@ Should we get one of these added for you so you're covered from the moment your 
             { id: 'totalpricecredit', type: 'text', label: 'Watch Price(£)' },
 
         ],
-        baseText: `Your credit check is complete and you have passed for [linecountcredit] amount of lines! As you are a valued customer and taking out the [creditdevice] we can do a watch to pair with this for only £[totalpricecredit] monthly. Would you like to know more about this at the end of this phone upgrade?`
+        baseText: `Your credit check is complete and you have passed for [linecountcredit] lines! As you are a valued customer and taking out the [creditdevice] we can do a watch to pair with this for only £[totalpricecredit] monthly. Would you like to know more about this at the end of this phone upgrade?`
     },
    
     {
@@ -591,6 +591,7 @@ copyButton.addEventListener('click', () => {
         console.error('Failed to copy: ', err);
     });
 });
+
 
 
 
